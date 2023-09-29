8086aab890de3b807ec43b9c6f898c8be884eca4
Jose Diaz <jldiaz1612@gmail.com>
2023-09-29 16:13:29 -0500

    Add lib to publish all modules

--- src/domain/pizza_order.rs
+++ src/domain/pizza_order.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct PizzaOrder {
     pub id: i32,
     pub size: String,
--- src/domain/user.rs
+++ src/domain/user.rs
@@ -1,3 +1,6 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct User {
     pub id: i32,
     pub name: String,
